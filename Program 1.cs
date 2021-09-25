4f6fdca543d066df5581a598a4a117de05dabc64
Valerii Dekhtiuk <dekhtiuk.v.s@gmail.com>
2021-09-25 15:06:38 +0300

    firstCommit

---
+++ Program 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+
+namespace Biggly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            UseBigInteger();
+            
+        }
+      static void  UseBigInteger ()
+        {
+            BigInteger biggly = BigInteger.Parse("999999999999999999999");
+            Console.WriteLine("Bigly is: {0}", biggly);
+            Console.WriteLine("Is even: {0}", biggly.IsEven);
+            Console.WriteLine("Power of two : {0}", biggly.IsPowerOfTwo);
+            BigInteger veryBig = BigInteger.Multiply(biggly, BigInteger.Parse("8888888888888888888"));
+                 Console.WriteLine(" the end is: {0}", veryBig);
+        }
+    }
+}
